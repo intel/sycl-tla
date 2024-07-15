e7381bc7161af529069fb29d56beabfe534a85c1
Jiaxingla <jiaxing.wen@intel.com>
2024-07-15 01:27:21 -0700

    rm redundancy code

--- include/cutlass/relatively_equal.h
+++ include/cutlass/relatively_equal.h
@@ -71,7 +71,7 @@ bool relatively_equal_float(T a, T b, T epsilon, T nonzero_floor) {
     return true;
   }
   else if (a == zero || b == zero || diff < nonzero_floor) {
-    return diff < (epsilon * nonzero_floor) || (diff / abs_B) < (T)0.001f;
+    return diff < (epsilon * nonzero_floor);
   }
   
   return diff < epsilon * (abs_A + abs_B);
