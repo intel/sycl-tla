1fa7d7348c35436aff82bcb85f7c11c3955e2ee0
atharva.dubey <atharva.dubey@codeplay.com>
2024-07-31 10:09:40 +0100

    Revert "disable global offset iff target is nvptx64-nvidia-cuda" Reverting as the same changes come in as a part of #104 This reverts commit bbb4defb57ac2fb88ad6992b0bfeb62ee4f037c4.

--- cmake/FindDPCPP.cmake
+++ cmake/FindDPCPP.cmake
@@ -38,7 +38,7 @@ find_library(DPCPP_LIB_DIR NAMES sycl sycl6 PATHS "${DPCPP_BIN_DIR}/../lib")
 
 add_library(DPCPP::DPCPP INTERFACE IMPORTED)
 
-set(DPCPP_FLAGS "-fsycl;")
+set(DPCPP_FLAGS "-fsycl;-mllvm;-enable-global-offset=false;")
 if(NOT "${DPCPP_SYCL_TARGET}" STREQUAL "")
   list(APPEND DPCPP_FLAGS "-fsycl-targets=${DPCPP_SYCL_TARGET};")
 endif()
@@ -48,7 +48,6 @@ if(NOT "${DPCPP_SYCL_ARCH}" STREQUAL "")
   if("${DPCPP_SYCL_TARGET}" STREQUAL "nvptx64-nvidia-cuda")
     list(APPEND DPCPP_FLAGS "-Xsycl-target-backend")
     list(APPEND DPCPP_FLAGS "--cuda-gpu-arch=${DPCPP_SYCL_ARCH}")
-    list(APPEND DPCPP_FLAGS "-mllvm;-enable-global-offset=false;")
   endif()
 endif()
 
