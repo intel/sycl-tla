6fccaa2684199574acba083a929128432b81ffda
Joe Todd <joe.todd@codeplay.com>
2024-11-19 11:25:08 +0000

    Xe LinCombEltAct unit test

--- test/unit/gemm/device/xe_gemm_f8_f8_bf16_tensor_op_fp32_evt.cpp
+++ test/unit/gemm/device/xe_gemm_f8_f8_bf16_tensor_op_fp32_evt.cpp
@@ -30,46 +30,31 @@
  **************************************************************************************************/
 
 /*! \file
-    \brief Tests for Sm90 f8_f8_bf16 with EVT epilogue 
-    ScaledLinCombPerRowBiasEltAct and ScaledLinCombPerRowBiasEltActAmaxAux
+    \brief Tests for Xe bf16t_bf16t_f32 with EVT epilogue
+    LinCombEltAct
 */
 
 #include <iostream>
 
 #include "cutlass/cutlass.h"
-#include "cute/tensor.hpp"
-#include "cute/atom/mma_atom.hpp"
-
-#include "cutlass/numeric_types.h"
 
 #include "cutlass/gemm/device/gemm_universal_adapter.h"
 #include "cutlass/gemm/kernel/gemm_universal.hpp"
 #include "cutlass/epilogue/collective/collective_builder.hpp"
 #include "cutlass/gemm/collective/collective_builder.hpp"
-#include "cutlass/epilogue/collective/xe_epilogue.hpp"
-#include "cutlass/epilogue/collective/default_epilogue.hpp"
-#include "cutlass/epilogue/thread/linear_combination.h"
-#include "cutlass/epilogue/thread/linear_combination_bias_elementwise.h"
 
-#include "../../common/cutlass_unit_test.h"
-
-#include "gemm_testbed_3x_evt.hpp"
-#include "sm90_evt_operations.hpp"
-
-
+#include "gemm_testbed_3x.hpp"
 
 using namespace cute;
 
-// Z = scale_a * scale_b * alpha * acc + beta * scale_c * C + per-row bias
-// if D is fp8 
-//   D = scale_d * activation(Z)
-// else
-//   D = activation(Z)
-TEST(XE_Device_Gemm_e4m3t_e4m3n_bf16t_tensor_op_gmma_f32_epilogue, 64x128x128_ScaledLinCombPerRowBiasEltAct) {
+// D = activation(alpha * acc + beta * C)
+TEST(XE_Device_Gemm_bf16t_bf16t_f32_tensor_op_gmma_f32_epilogue, 256x256x32_LinCombEltAct) {
   using LayoutA = cutlass::layout::RowMajor;
   using LayoutB = cutlass::layout::RowMajor;
   using LayoutC = cutlass::layout::RowMajor;
-  using TileShape_MNK = Shape<_64,_128,_128>;
+  using LayoutD = cutlass::layout::RowMajor;
+
+  using TileShape_MNK = Shape<_256, _256, _32>;
   using ClusterShape_MNK = Shape<_1,_1,_1>;
 
   using EpilogueSchedule = cutlass::epilogue::collective::EpilogueScheduleAuto;
@@ -79,7 +64,11 @@ TEST(XE_Device_Gemm_e4m3t_e4m3n_bf16t_tensor_op_gmma_f32_epilogue, 64x128x128_Sc
   using ElementInputB = bfloat16_t;
   using ElementOutput = float;
 
-  //TODO(joe): Do LinCombPerRowBias...
+  constexpr int AlignmentA = sizeof(ElementInputA);
+  constexpr int AlignmentB = sizeof(ElementInputB);
+  constexpr int AlignmentC = sizeof(ElementAccumulator);
+  constexpr int AlignmentD = sizeof(ElementOutput);
+
   using FusionCallbacks = cutlass::epilogue::fusion::LinCombEltAct<cutlass::epilogue::thread::ReLu, 
           ElementOutput, ElementComputeEpilogue, ElementAccumulator, 
           ElementAccumulator, cutlass::FloatRoundStyle::round_to_nearest>;
@@ -89,16 +78,16 @@ TEST(XE_Device_Gemm_e4m3t_e4m3n_bf16t_tensor_op_gmma_f32_epilogue, 64x128x128_Sc
       TileShape_MNK, ClusterShape_MNK,
       cutlass::epilogue::collective::EpilogueTileAuto,
       ElementComputeEpilogue, ElementAccumulator,
-      ElementAccumulator, LayoutC, 8,
-      ElementOutput, LayoutC, 8,
+      ElementAccumulator, LayoutC, AlignmentC,
+      ElementOutput, LayoutD, AlignmentD,
       EpilogueSchedule,
       FusionCallbacks
     >::CollectiveOp;
 
   using CollectiveMainloop = typename cutlass::gemm::collective::CollectiveBuilder<
       cutlass::arch::IntelPVC, cutlass::arch::OpClassTensorOp,
-      ElementInputA, LayoutA, 16,
-      ElementInputB, LayoutB, 16,
+      ElementInputA, LayoutA, AlignmentA,
+      ElementInputB, LayoutB, AlignmentB,
       ElementAccumulator,
       TileShape_MNK, ClusterShape_MNK,
       cutlass::gemm::collective::StageCountAuto,
@@ -113,86 +102,7 @@ TEST(XE_Device_Gemm_e4m3t_e4m3n_bf16t_tensor_op_gmma_f32_epilogue, 64x128x128_Sc
 
   using Gemm = cutlass::gemm::device::GemmUniversalAdapter<GemmKernel>;
 
-  // Host reference
-  using HostReference = test::gemm::device::HostScaledLinCombPerRowBiasEltAct<
-    Gemm, cutlass::epilogue::thread::ReLu, cutlass::bfloat16_t
-  >;
-  bool passed = test::gemm::device::TestAllEVT<Gemm, HostReference>(true);
+  bool passed = test::gemm::device::TestSingleShapeXe<Gemm>(1.0, 0.0);
   EXPECT_TRUE(passed);
 }
 
-// Z = scale_a * scale_b * alpha * acc + scale_c * beta * C + per-row bias
-// if D is fp8 
-//   amax_d = max(abs(elements in activation(Z)))
-//   D = scale_d * activation(Z)
-// else
-//   D = activation(Z)
-// if Aux is fp8 
-//   amax_aux = max(abs(elements in Z))
-//   Aux = scale_aux * Z
-// else
-//   Aux = Z
-// TEST(XE_Device_Gemm_e4m3t_e4m3n_bf16n_tensor_op_gmma_f32_epilogue, 64x128x128_4x1x1_LinCombEltAct) {
-//   using LayoutA = cutlass::layout::RowMajor;
-//   using LayoutB = cutlass::layout::ColumnMajor;
-//   using LayoutC = cutlass::layout::ColumnMajor;
-//   using TileShape_MNK = Shape<_64,_128,_128>;
-//   using ClusterShape_MNK = Shape<_2,_4,_1>;
-//
-//   using EpilogueSchedule = cutlass::epilogue::collective::EpilogueScheduleAuto;
-//   using EpilogueTileType = cutlass::epilogue::collective::EpilogueTileAuto;
-//   using EpilogueDescriptor = cutlass::epilogue::collective::detail::EpilogueDescriptor<
-//     TileShape_MNK, EpilogueTileType, cutlass::bfloat16_t, cutlass::bfloat16_t, EpilogueSchedule>;
-//   using AuxStoreDescriptor = cutlass::epilogue::collective::detail::AuxStoreDescriptor<
-//     EpilogueDescriptor, cutlass::layout::RowMajor, cutlass::bfloat16_t>;
-//
-//   using FusionCallbacks = cutlass::epilogue::fusion::Sm90LinCombEltAct<
-//     TileShape_MNK,                               // CtaTileShapeMNK
-//     typename EpilogueDescriptor::EpilogueTile,   // EpilogueTile
-//     EpilogueDescriptor::StagesD,                 // StagesD
-//     typename AuxStoreDescriptor::Stride,         // StrideAux
-//     typename AuxStoreDescriptor::SmemLayoutAtom, // SmemLayoutAtom
-//     typename AuxStoreDescriptor::CopyOpR2S,      // CopyOpR2S
-//     cutlass::epilogue::thread::ReLu,             // ActivationFn
-//     cutlass::bfloat16_t,                         // ElementOutput
-//     float,                                       // ElementCompute
-//     cutlass::bfloat16_t,                         // ElementBias
-//     float                                        // ElementScalar
-//   >;
-//
-//   using CollectiveEpilogue = typename cutlass::epilogue::collective::CollectiveBuilder<
-//       cutlass::arch::IntelPVC, cutlass::arch::OpClassTensorOp,
-//       TileShape_MNK, ClusterShape_MNK,
-//       EpilogueTileType,
-//       float, float,
-//       cutlass::bfloat16_t, LayoutC, 16,
-//       cutlass::bfloat16_t, LayoutC, 16,
-//       EpilogueSchedule,
-//       FusionCallbacks
-//     >::CollectiveOp;
-//
-//   using CollectiveMainloop = typename cutlass::gemm::collective::CollectiveBuilder<
-//       cutlass::arch::IntelPVC, cutlass::arch::OpClassTensorOp,
-//       cutlass::float_e4m3_t, LayoutA, 16,
-//       cutlass::float_e4m3_t, LayoutB, 16,
-//       float,
-//       TileShape_MNK, ClusterShape_MNK,
-//       cutlass::gemm::collective::StageCountAuto,
-//       cutlass::gemm::collective::KernelScheduleAuto
-//     >::CollectiveOp;
-//
-//   using GemmKernel = cutlass::gemm::kernel::GemmUniversal<
-//       Shape<int,int,int,int>,
-//       CollectiveMainloop,
-//       CollectiveEpilogue
-//   >;
-//
-//   using Gemm = cutlass::gemm::device::GemmUniversalAdapter<GemmKernel>;
-//
-//   // Host reference
-//   using HostReference = test::gemm::device::HostLinCombEltAct<
-//     Gemm, cutlass::epilogue::thread::ReLu, cutlass::bfloat16_t
-//   >;
-//   bool passed = test::gemm::device::TestAllEVT<Gemm, HostReference>(true);
-//   EXPECT_TRUE(passed);
-// }
