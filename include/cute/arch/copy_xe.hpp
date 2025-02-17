f4aa5eff948fb6708a47ab6842c45b93260b5d71
Joe Todd <joe.todd@codeplay.com>
2025-02-17 21:09:08 +0000

    Replace sg.load/store with group_load and group_store

--- include/cute/arch/copy_xe.hpp
+++ include/cute/arch/copy_xe.hpp
@@ -132,9 +132,7 @@ struct XE_1D_LDSM {
     #if defined(SYCL_INTEL_TARGET)
       CUTE_STATIC_ASSERT(sizeof(S_) == sizeof(S));
       auto sg = sycl::ext::oneapi::this_work_item::get_nd_item<3>().get_sub_group();
-      *(sycl::vec<S_, N>*)(&dst)
-        = sg.load<N>(sycl::address_space_cast<sycl::access::address_space::local_space,
-                  sycl::access::decorated::yes>(&*&src));
+      sycl::ext::oneapi::experimental::group_load(sg, &*&src, *(sycl::vec<S_, N>*)(&dst));
     #else
       CUTE_INVALID_CONTROL_PATH("Trying to use block loads on non-PVC hardware");
     #endif 
@@ -202,9 +200,7 @@ struct XE_1D_LOAD_GLOBAL {
       CUTE_STATIC_ASSERT(sizeof(S_) == sizeof(S));
       CUTE_STATIC_ASSERT(sizeof(D_) == sizeof(D));
       auto sg = sycl::ext::oneapi::this_work_item::get_nd_item<3>().get_sub_group();
-      *(sycl::vec<S_, N>*)(&dst) 
-        = sg.load<N>(sycl::address_space_cast<sycl::access::address_space::global_space,
-                  sycl::access::decorated::yes>(&*&src));
+      sycl::ext::oneapi::experimental::group_load(sg, &*&src, *(sycl::vec<S_, N>*)(&dst));
     #else
       CUTE_INVALID_CONTROL_PATH("Trying to use block loads on non-PVC hardware");
     #endif 
@@ -230,8 +226,7 @@ struct XE_1D_STSM {
   copy(S_ const& src, D_ & dst) {
     #if defined(SYCL_INTEL_TARGET)
       auto sg = sycl::ext::oneapi::this_work_item::get_nd_item<3>().get_sub_group();
-      sg.store<N>(sycl::address_space_cast<sycl::access::address_space::local_space,
-            sycl::access::decorated::yes>(&*&dst), *(sycl::vec<D_, N>*)(&src));
+      sycl::ext::oneapi::experimental::group_store(sg, *(sycl::vec<D_, N>*)(&src), &*&dst);
     #else
       CUTE_INVALID_CONTROL_PATH("Trying to use block loads on non-PVC hardware");
     #endif
@@ -254,8 +249,7 @@ struct XE_1D_STORE_GLOBAL {
   copy(S_ const& src, D_ &dst) {
     #if defined(SYCL_INTEL_TARGET)
       auto sg = sycl::ext::oneapi::this_work_item::get_nd_item<3>().get_sub_group();
-      sg.store<N>(sycl::address_space_cast<sycl::access::address_space::global_space,
-            sycl::access::decorated::yes>(&*&dst), *(sycl::vec<D_, N>*)(&src));
+      sycl::ext::oneapi::experimental::group_store(sg, *(sycl::vec<D_, N>*)(&src), &*&dst);
     #else
       CUTE_INVALID_CONTROL_PATH("Trying to use block loads on non-PVC hardware");
     #endif
