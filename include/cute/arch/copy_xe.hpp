5728f209c5cbe802575ffe575ecc0bbbcf4a72b2
Joe Todd <joe.todd@codeplay.com>
2025-02-19 14:40:53 +0000

    Use reinterpret_cast, and pass perf-related properties

--- include/cute/arch/copy_xe.hpp
+++ include/cute/arch/copy_xe.hpp
@@ -131,8 +131,13 @@ struct XE_1D_LDSM {
   copy(const S_ &src, D_ &dst) {
     #if defined(SYCL_INTEL_TARGET)
       CUTE_STATIC_ASSERT(sizeof(S_) == sizeof(S));
-      auto sg = sycl::ext::oneapi::this_work_item::get_nd_item<3>().get_sub_group();
-      sycl::ext::oneapi::experimental::group_load(sg, &*&src, *(sycl::vec<S_, N>*)(&dst));
+      auto props = sycl::ext::oneapi::experimental::properties{
+          sycl::ext::oneapi::experimental::contiguous_memory,
+          sycl::ext::oneapi::experimental::alignment<sizeof(D)>};
+      auto sg =
+          sycl::ext::oneapi::this_work_item::get_nd_item<3>().get_sub_group();
+      sycl::ext::oneapi::experimental::group_load(
+          sg, &*&src, *reinterpret_cast<sycl::vec<S_, N> *>(&dst), props);
     #else
       CUTE_INVALID_CONTROL_PATH("Trying to use block loads on non-PVC hardware");
     #endif 
@@ -200,7 +205,11 @@ struct XE_1D_LOAD_GLOBAL {
       CUTE_STATIC_ASSERT(sizeof(S_) == sizeof(S));
       CUTE_STATIC_ASSERT(sizeof(D_) == sizeof(D));
       auto sg = sycl::ext::oneapi::this_work_item::get_nd_item<3>().get_sub_group();
-      sycl::ext::oneapi::experimental::group_load(sg, &*&src, *(sycl::vec<S_, N>*)(&dst));
+      auto props = sycl::ext::oneapi::experimental::properties{
+          sycl::ext::oneapi::experimental::contiguous_memory,
+          sycl::ext::oneapi::experimental::alignment<sizeof(D)>};
+      sycl::ext::oneapi::experimental::group_load(
+          sg, &*&src, *reinterpret_cast<sycl::vec<S_, N> *>(&dst), props);
     #else
       CUTE_INVALID_CONTROL_PATH("Trying to use block loads on non-PVC hardware");
     #endif 
@@ -226,7 +235,11 @@ struct XE_1D_STSM {
   copy(S_ const& src, D_ & dst) {
     #if defined(SYCL_INTEL_TARGET)
       auto sg = sycl::ext::oneapi::this_work_item::get_nd_item<3>().get_sub_group();
-      sycl::ext::oneapi::experimental::group_store(sg, *(sycl::vec<D_, N>*)(&src), &*&dst);
+      auto props = sycl::ext::oneapi::experimental::properties{
+          sycl::ext::oneapi::experimental::contiguous_memory,
+          sycl::ext::oneapi::experimental::alignment<sizeof(S)>};
+      sycl::ext::oneapi::experimental::group_store(
+          sg, *reinterpret_cast<sycl::vec<D_, N> const *>(&src), &*&dst, props);
     #else
       CUTE_INVALID_CONTROL_PATH("Trying to use block loads on non-PVC hardware");
     #endif
@@ -249,7 +262,11 @@ struct XE_1D_STORE_GLOBAL {
   copy(S_ const& src, D_ &dst) {
     #if defined(SYCL_INTEL_TARGET)
       auto sg = sycl::ext::oneapi::this_work_item::get_nd_item<3>().get_sub_group();
-      sycl::ext::oneapi::experimental::group_store(sg, *(sycl::vec<D_, N>*)(&src), &*&dst);
+      auto props = sycl::ext::oneapi::experimental::properties{
+          sycl::ext::oneapi::experimental::contiguous_memory,
+          sycl::ext::oneapi::experimental::alignment<sizeof(S)>};
+      sycl::ext::oneapi::experimental::group_store(
+          sg, *reinterpret_cast<sycl::vec<D_, N> const *>(&src), &*&dst, props);
     #else
       CUTE_INVALID_CONTROL_PATH("Trying to use block loads on non-PVC hardware");
     #endif
