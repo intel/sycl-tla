3b3f296e33be668176ee3211011e0047e7d86146
Joe Todd <joe.todd@codeplay.com>
2025-03-03 09:10:04 +0000

    Replace removed bfloat16ToBits with bit_cast

--- include/cutlass/bfloat16.h
+++ include/cutlass/bfloat16.h
@@ -126,7 +126,7 @@ public:
 
     #elif defined(CUTLASS_ENABLE_SYCL)
 
-    storage = sycl::ext::oneapi::detail::bfloat16ToBits(sycl::ext::oneapi::bfloat16(x));
+    storage = bit_cast<decltype(storage)>(sycl::ext::oneapi::bfloat16(x));
 
     #else
     uint32_t bits;
