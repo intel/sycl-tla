5ae93ccc75a06263bccc2a79582189fe9cc3ccb9
Joe Todd <joe.todd@codeplay.com>
2025-03-03 09:45:44 +0000

    Use cutlass::platform::bit_cast (i.e. reinterpret_cast)
    since std::bit_cast is C++20

--- include/cutlass/bfloat16.h
+++ include/cutlass/bfloat16.h
@@ -126,7 +126,7 @@ public:
 
     #elif defined(CUTLASS_ENABLE_SYCL)
 
-    storage = std::bit_cast<decltype(storage)>(sycl::ext::oneapi::bfloat16(x));
+    storage = cutlass::platform::bit_cast<decltype(storage)>(sycl::ext::oneapi::bfloat16(x));
 
     #else
     uint32_t bits;
