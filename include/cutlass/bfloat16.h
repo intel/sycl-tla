f9fdd32eb34426404aca748d4461dac574f892ea
Joe Todd <joe.todd@codeplay.com>
2025-03-03 09:26:38 +0000

    Update include/cutlass/bfloat16.h
    Thanks Tadej!
    
    Co-authored-by: Tadej Ciglarič <tadej.c@gmail.com>

--- include/cutlass/bfloat16.h
+++ include/cutlass/bfloat16.h
@@ -126,7 +126,7 @@ public:
 
     #elif defined(CUTLASS_ENABLE_SYCL)
 
-    storage = bit_cast<decltype(storage)>(sycl::ext::oneapi::bfloat16(x));
+    storage = std::bit_cast<decltype(storage)>(sycl::ext::oneapi::bfloat16(x));
 
     #else
     uint32_t bits;
