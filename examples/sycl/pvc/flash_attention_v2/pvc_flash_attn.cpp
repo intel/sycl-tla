c01782bf6455e5b0c18f24b1b1ae86ab1814ae9c
mehdi-goli <mehdi.goli@codeplay.com>
2025-03-10 20:21:27 +0000

    Reducing the subgroupsize for smaller size problem

--- examples/sycl/pvc/flash_attention_v2/pvc_flash_attn.cpp
+++ examples/sycl/pvc/flash_attention_v2/pvc_flash_attn.cpp
@@ -50,7 +50,7 @@ int main(int argc, const char **argv) {
     return -1;
   }
 
-  if (options.head_size == 64) {
+  if (options.head_size == 64 || options.head_size == 96) {
 
     using TiledMma = TiledMMA<MMA_Atom<XE_8x16x16_F32BF16BF16F32_TT>, Layout<Shape<_8, _1, _1>, Stride<_1, _1, _1>>,
                               // Atom, Hardware(NUMBER OF CONCURRENT MMA), Iteration
@@ -61,7 +61,7 @@ int main(int argc, const char **argv) {
                                    _64>>;                                            // K is going to be 64
     return options.is_causal ? FMHAConfig<true, Shape<_128, _64, _64>, TiledMma>::run(options)
                              : FMHAConfig<false, Shape<_128, _64, _64>, TiledMma>::run(options);
-  } else if (options.head_size == 96 || options.head_size == 192) {
+  } else if (options.head_size == 192) {
 
     using TiledMma = TiledMMA<MMA_Atom<XE_8x16x16_F32BF16BF16F32_TT>, Layout<Shape<_16, _1, _1>, Stride<_1, _1, _1>>,
                               // Atom, Hardware(NUMBER OF CONCURRENT MMA), Iteration
