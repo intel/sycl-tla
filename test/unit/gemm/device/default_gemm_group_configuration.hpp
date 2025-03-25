b96f6f045c018f31679b38b911a55f62584a3499
Muhammad Tanvir <muhammad.tanvir@codeplay.com>
2025-03-25 06:50:36 +0000

    Use TiledMmaHelper for Group GEMM tests

--- test/unit/gemm/device/default_gemm_group_configuration.hpp
+++ test/unit/gemm/device/default_gemm_group_configuration.hpp
@@ -72,11 +72,9 @@ struct DefaultGemmGroupConfiguration<
 {
   using TileShape = Shape<_256, _256, _32>;
 
-  using TiledMma =
-      TiledMMA<MMA_Atom<XE_8x16x16_F32BF16BF16F32_TT>,
-               Layout<Shape<_8, _4, _1>, Stride<_4, _1, _0>>,
-               Tile<Layout<Shape<_8, _8, _4>, Stride<_1, _32, _8>>,
-                    Layout<Shape<_16, _4, _4>, Stride<_1, _64, _16>>, _32>>;
+  using TiledMma = typename TiledMMAHelper<MMA_Atom<XE_8x16x16_F32BF16BF16F32_TT>,
+                                          Layout<TileShape>,
+                                          Layout<Shape<_8, _4, _1>, Stride<_4, _1, _0>>>::TiledMMA;
 
   // A
   static constexpr int kAlignmentA = 32;
@@ -129,11 +127,9 @@ struct DefaultGemmGroupConfiguration<
 {
   using TileShape = Shape<_256, _256, _32>;
 
-  using TiledMma =
-      TiledMMA<MMA_Atom<XE_8x16x16_F32F16F16F32_TT>,
-               Layout<Shape<_8, _4, _1>, Stride<_4, _1, _0>>,
-               Tile<Layout<Shape<_8, _8, _4>, Stride<_1, _32, _8>>,
-                    Layout<Shape<_16, _4, _4>, Stride<_1, _64, _16>>, _32>>;
+  using TiledMma = typename TiledMMAHelper<MMA_Atom<XE_8x16x16_F32F16F16F32_TT>,
+                                          Layout<TileShape>,
+                                          Layout<Shape<_8, _4, _1>, Stride<_4, _1, _0>>>::TiledMMA;
 
   // A
   static constexpr int kAlignmentA = 32;
