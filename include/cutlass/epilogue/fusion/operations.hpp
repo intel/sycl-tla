609d94972b3cf9b4bbadbe7f2a0b9b49e3723d22
mehdi-goli <mehdi.goli@codeplay.com>
2025-04-01 22:01:40 +0100

    Generalising dual gemm

--- include/cutlass/epilogue/fusion/operations.hpp
+++ include/cutlass/epilogue/fusion/operations.hpp
@@ -371,6 +371,7 @@ struct ScaledLinCombPerColBiasEltAct
   static constexpr bool IsScaleFactorSupported = true;
 };
 
+
 // Z = scale_a * scale_b * alpha * acc + scale_c * beta * C + per-row bias
 // if D is fp8 
 //   amax_d = max(abs(elements in activation(Z)))
