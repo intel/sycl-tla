48e14ad58c6d219f28a07978f9c329f4b72f2f4e
Mehdi Goli <mehdi.goli@codeplay.com>
2025-04-16 10:35:50 +0100

    Update benchmarks/pvc/flash_attention_v2/benchmark_runner.hpp
    Co-authored-by: Muhammad Tanvir <muhammad.tanvir@codeplay.com>

--- benchmarks/pvc/flash_attention_v2/benchmark_runner.hpp
+++ benchmarks/pvc/flash_attention_v2/benchmark_runner.hpp
@@ -76,7 +76,7 @@ struct FMHAOptions {
 
     cmd.get_cmd_line_argument("batch", batch, 32);
     cmd.get_cmd_line_argument("num_heads_q", num_heads_q, 16);
-    cmd.get_cmd_line_argument("num_heads_kv", num_heads_kv, 16);
+    cmd.get_cmd_line_argument("num_heads_kv", num_heads_kv, num_heads_q);
     cmd.get_cmd_line_argument("seq_len_kv", seq_len_kv, seq_len_qo);
     cmd.get_cmd_line_argument("head_size_vo", head_size_vo, 128);
     cmd.get_cmd_line_argument("head_size_qk", head_size_qk, head_size_vo);
