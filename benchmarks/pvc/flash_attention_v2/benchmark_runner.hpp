9b3c761b0a53b19c301446d588dd7d58a0a2665d
mehdi-goli <mehdi.goli@codeplay.com>
2025-04-29 22:08:41 +0100

    Applying the same change for the cahedKV

--- benchmarks/pvc/flash_attention_v2/benchmark_runner.hpp
+++ benchmarks/pvc/flash_attention_v2/benchmark_runner.hpp
@@ -546,7 +546,8 @@ template <class FMHAConfiguration> struct BenchmarkRunnerFMHA {
     double flops_qk = 2.0 * options.batch * options.num_heads_q * effective_seq_len_qo * effective_seq_len_kv * options.head_size_qk;
     double flops_pv = 2.0 * options.batch * options.num_heads_q * effective_seq_len_qo * options.head_size_vo * effective_seq_len_kv;
     double gflops = (flops_qk + flops_pv) * 1e-9;
-    double gbps_qk = sizeof(ElementQ) * options.batch * (options.num_heads_q * effective_seq_len_qo * options.head_size_qk + options.num_heads_kv * effective_seq_len_kv * options.head_size_qk);
+    double gbps_qk =  options.batch * (sizeof(ElementQ) * options.num_heads_q * effective_seq_len_qo * options.head_size_qk + 
+                      sizeof(ElementK) * options.num_heads_kv * effective_seq_len_kv * options.head_size_qk);    
     double gbps_pv = sizeof(ElementV) * options.batch * options.num_heads_kv * effective_seq_len_kv * options.head_size_vo +
                      sizeof(ElementOutput) * options.batch * options.num_heads_q * effective_seq_len_qo * options.head_size_vo;
     double mega_bytes_transferred = (gbps_qk + gbps_pv) * (1e-6);
--- examples/sycl/06_pvc_flash_attention/pvc_flash_attn_runner.hpp
+++ examples/sycl/06_pvc_flash_attention/pvc_flash_attn_runner.hpp
@@ -542,7 +542,8 @@ template <class GemmKernel, bool isVarLen> struct ExampleRunner {
       double flops_qk = 2.0 * options.batch * options.num_heads_q * effective_seq_len_qo * effective_seq_len_kv * options.head_size_qk;
       double flops_pv = 2.0 *  options.batch * options.num_heads_q * effective_seq_len_qo * options.head_size_vo * effective_seq_len_kv;
       double tflops = ((flops_qk + flops_pv) * 1e-12) / cute_time;
-      double gbps_qk = sizeof(ElementQ) * options.batch * (options.num_heads_q * effective_seq_len_qo * options.head_size_qk + options.num_heads_kv * effective_seq_len_kv * options.head_size_qk);
+      double gbps_qk =  options.batch * (sizeof(ElementQ) * options.num_heads_q * effective_seq_len_qo * options.head_size_qk + 
+                                         sizeof(ElementK) * options.num_heads_kv * effective_seq_len_kv * options.head_size_qk);
       double gbps_pv = sizeof(ElementV) * options.batch * options.num_heads_kv * effective_seq_len_kv * options.head_size_vo +
                      sizeof(ElementOutput) * options.batch * options.num_heads_q * effective_seq_len_qo * options.head_size_vo;
       double gbps = ((gbps_qk + gbps_pv)  * 1e-9) / (cute_time);
