b962239ed998d236fba26fd51a9adfe9ecabbbb9
Alejandro Acosta <alejandro.acosta@codeplay.com>
2025-05-27 15:29:45 +0100

    fix merge conflict

--- include/cute/arch/copy_xe_U8.hpp
+++ include/cute/arch/copy_xe_U8.hpp
@@ -223,7 +223,7 @@ struct XE_2D_U8x32x32_LD_N {
                                     T *dst) {
 #if defined(CUTE_ARCH_COPY_XE_ENABLED)
     static_assert(sizeof(T) == 1, "Expected T to have size 1");
-    detail::XeSubgroup2DBlockLoad<1, 32, 32, 1>{}(baseoffset, width, height, pitch, coord, dst);
+    detail::XeSubgroup2DBlockLoad<1, 16, 32, 2>{}(baseoffset, width, height, pitch, coord, dst);
 #else
     CUTE_INVALID_CONTROL_PATH("Trying to use block loads on non-Xe hardware");
 #endif
