d247c65d28fdbba8376237f082e41a8fa1bd37d8
sanchitintel <sanchit.jain@intel.com>
2025-05-28 22:56:21 -0700

    Format fp8_to_fp16.h with clang-format

--- include/cutlass/fp8_to_fp16.h
+++ include/cutlass/fp8_to_fp16.h
@@ -29,35 +29,35 @@
  *
  **************************************************************************************************/
 
- #pragma once
+#pragma once
 
-#include <cutlass/half.h>
-#include <cutlass/detail/helper_macros.hpp>
-#include <cute/tensor_impl.hpp>
-#include <cute/pointer.hpp>
-#include <cute/underscore.hpp>
 #include <cute/layout.hpp>
 #include <cute/numeric/numeric_types.hpp>
+#include <cute/pointer.hpp>
+#include <cute/tensor_impl.hpp>
+#include <cute/underscore.hpp>
 #include <cute/util/sycl_vec.hpp>
+#include <cutlass/detail/helper_macros.hpp>
+#include <cutlass/half.h>
 
 using uchar16 = cute::intel::uchar16;
 using ushort16 = cute::intel::ushort16;
 
 static inline ushort16 convert_ushort16(uchar16 x) {
-    ushort16 result;
-    #pragma unroll
-    for (int i = 0; i < 16; ++i) {
-        result[i] = static_cast<uint16_t>(x[i]);
-    }
-    return result;
+  ushort16 result;
+#pragma unroll
+  for (int i = 0; i < 16; ++i) {
+    result[i] = static_cast<uint16_t>(x[i]);
+  }
+  return result;
 }
 
 static inline unsigned short E4M3_to_FP16(unsigned char xin) {
   unsigned char xa, sgn_x, nan_mask, den_mask;
 
   union {
-      signed short i;
-      _Float16 f;
+    signed short i;
+    _Float16 f;
   } x16, den_corr;
 
   xa = xin & 0x7f;
@@ -92,16 +92,14 @@ static inline unsigned short E4M3_to_FP16(unsigned char xin) {
   return (unsigned short)x16.i;
 }
 
-
-
 static inline ushort16 E4M3_to_FP16_chunk16(uchar16 xin) {
   uchar16 xa = xin & 0x7F;
   uchar16 sgn_x = xin ^ xa;
 
   uchar16 zero_mask;
-  #pragma unroll
+#pragma unroll
   for (int i = 0; i < 16; ++i) {
-      zero_mask[i] = (xa[i] == 0) ? 1 : 0;
+    zero_mask[i] = (xa[i] == 0) ? 1 : 0;
   }
   uchar16 nan_mask = (0x7E - xa) & 0x80;
   uchar16 den_mask = ((xa - 8) >> 7) & 0x01;
@@ -123,24 +121,28 @@ static inline ushort16 E4M3_to_FP16_chunk16(uchar16 xin) {
   return result;
 }
 
-
 template <int N>
-static inline void E5M2_to_FP16(cutlass::Array<uint16_t, N> const &xin, cutlass::Array<uint32_t, N> &xout) {
-  // Adapted from https://github.com/pytorch/pytorch/blob/dfcfad2112933cc34247421ac0a4d3f19a1806c1/c10/util/Float8_e5m2.h#L30-L43,
-  // except that since 32-bit registers & int32 ALUs are used, convert 2 FP8 elements at a time.
+static inline void E5M2_to_FP16(cutlass::Array<uint16_t, N> const &xin,
+                                cutlass::Array<uint32_t, N> &xout) {
+  // Adapted from
+  // https://github.com/pytorch/pytorch/blob/dfcfad2112933cc34247421ac0a4d3f19a1806c1/c10/util/Float8_e5m2.h#L30-L43,
+  // except that since 32-bit registers & int32 ALUs are used, convert 2 FP8
+  // elements at a time.
   CUTLASS_PRAGMA_UNROLL
   for (int i = 0; i < N; i += 4) {
     // Manually unroll since using CUTLASS_PRAGMA_UNROLL or #pragma unroll
     // is not working here with DPCPP nightly dated March 24.
-    // More recent DPCPP nightlies were not working with cutlass at the time this code was written.
-    // If preprocessor directive for loop unrolling worked, we could've determined the unrolling factor at compile time,
+    // More recent DPCPP nightlies were not working with cutlass at the time
+    // this code was written. If preprocessor directive for loop unrolling
+    // worked, we could've determined the unrolling factor at compile time,
     // based on how many elements each work-item handled.
-    // An unrolling factor of 8 would be desirable (although shl instruction can pipeline 9 inputs at a time,
-    // the inputs we usually handle are not a multiple of 9).
-    // but this code uses an unrolling factor of 4 so that it can work with a multiple of 8 FP8 elements.
-    // The subgroup tile to be converted in that case would have a multiple of 128 elements,
-    // so we would also be able to cover small subgroup-level tiles such as 8x16,
-    // which would not have been possible with an unrolling factor of 8.
+    // An unrolling factor of 8 would be desirable (although shl instruction can
+    // pipeline 9 inputs at a time, the inputs we usually handle are not a
+    // multiple of 9). but this code uses an unrolling factor of 4 so that it
+    // can work with a multiple of 8 FP8 elements. The subgroup tile to be
+    // converted in that case would have a multiple of 128 elements, so we would
+    // also be able to cover small subgroup-level tiles such as 8x16, which
+    // would not have been possible with an unrolling factor of 8.
     uint32_t tmp0 = static_cast<uint32_t>(xin[i]);
     uint32_t tmp1 = static_cast<uint32_t>(xin[i + 1]);
     uint32_t tmp2 = static_cast<uint32_t>(xin[i + 2]);
@@ -161,27 +163,26 @@ static inline void E5M2_to_FP16(cutlass::Array<uint16_t, N> const &xin, cutlass:
 }
 
 template <int N>
-static inline void E5M2_to_FP16(cutlass::Array<uint8_t, N> const &xin, cutlass::Array<uint16_t, N> &xout) {
-  // Adapted from https://github.com/pytorch/pytorch/blob/dfcfad2112933cc34247421ac0a4d3f19a1806c1/c10/util/Float8_e5m2.h#L30-L43
+static inline void E5M2_to_FP16(cutlass::Array<uint8_t, N> const &xin,
+                                cutlass::Array<uint16_t, N> &xout) {
+  // Adapted from
+  // https://github.com/pytorch/pytorch/blob/dfcfad2112933cc34247421ac0a4d3f19a1806c1/c10/util/Float8_e5m2.h#L30-L43
   CUTLASS_PRAGMA_UNROLL
   for (int i = 0; i < N; i++) {
     xout[i] = (static_cast<uint16_t>(xin[i])) << 8;
   }
 }
 
-template <typename ElementA,
-    class EngineIn,
-    class EngineOut,
-    class LayoutIn,
-    class LayoutOut,
-    class... Ts>
-CUTLASS_DEVICE
-void convert_FP8_to_FP16(
-        cute::Tensor<EngineIn, LayoutIn> const& in,
-        cute::Tensor<EngineOut, LayoutOut>& out) {
+template <typename ElementA, class EngineIn, class EngineOut, class LayoutIn,
+          class LayoutOut, class... Ts>
+CUTLASS_DEVICE void
+convert_FP8_to_FP16(cute::Tensor<EngineIn, LayoutIn> const &in,
+                    cute::Tensor<EngineOut, LayoutOut> &out) {
 
-  static_assert(cute::is_rmem<EngineIn>::value, "Input tensor for A conversion must come from registers");
-  static_assert(cute::is_rmem<EngineOut>::value, "Output tensor for A conversion must come from registers");
+  static_assert(cute::is_rmem<EngineIn>::value,
+                "Input tensor for A conversion must come from registers");
+  static_assert(cute::is_rmem<EngineOut>::value,
+                "Output tensor for A conversion must come from registers");
   static_assert(cute::cosize_v<LayoutIn> == cute::cosize_v<LayoutOut>);
   static_assert(cute::size_v<LayoutIn> == cute::cosize_v<LayoutIn>);
   static_assert(cute::size_v<LayoutOut> == cute::cosize_v<LayoutOut>);
@@ -189,14 +190,16 @@ void convert_FP8_to_FP16(
   using SrcType = typename EngineIn::value_type;
   using DstType = typename EngineOut::value_type;
 
-  static_assert(std::is_same_v<SrcType, uint8_t>, "Expected fp8 (E4M3) input as uint8_t");
-  static_assert(std::is_same_v<DstType, cute::half_t>, "Expected fp16 output as half_t");
+  static_assert(std::is_same_v<SrcType, uint8_t>,
+                "Expected fp8 (E4M3) input as uint8_t");
+  static_assert(std::is_same_v<DstType, cute::half_t>,
+                "Expected fp16 output as half_t");
 
-  auto const& src = in(cute::_, cute::_, cute::_);
-  auto const& dst = out(cute::_, cute::_, cute::_);
+  auto const &src = in(cute::_, cute::_, cute::_);
+  auto const &dst = out(cute::_, cute::_, cute::_);
 
-  SrcType const* pSrc = src.data();
-  DstType* pDst = dst.data();
+  SrcType const *pSrc = src.data();
+  DstType *pDst = dst.data();
 
   constexpr int num_elements = decltype(size(src))::value;
 
@@ -206,24 +209,23 @@ void convert_FP8_to_FP16(
     // leads to poor performance.
     // CUTLASS_PRAGMA_UNROLL
     // for (int i = 0; i < num_elements; i++) {
-    //   reinterpret_cast<uint16_t*>(pDst)[i] = (static_cast<uint16_t>((pSrc[i]))) << 8;
+    //   reinterpret_cast<uint16_t*>(pDst)[i] =
+    //   (static_cast<uint16_t>((pSrc[i]))) << 8;
     // }
     // The root-cause is unknown, but private memory use is seen in that case.
     // We're using a workaround that doesn't use private memory.
-    using src_dtype = std::conditional_t<num_elements >= 8,
-                      uint16_t,
-                      uint8_t>;
-    using dst_dtype = std::conditional_t<num_elements >= 8,
-                      uint32_t,
-                      uint16_t>;
-    using SrcArray = std::conditional_t<num_elements >= 8,
-                     cutlass::Array<src_dtype, num_elements / 2>,
-                     cutlass::Array<src_dtype, num_elements>>;
-    using DstArray = std::conditional_t<num_elements >= 8,
-                     cutlass::Array<dst_dtype, num_elements / 2>,
-                     cutlass::Array<dst_dtype, num_elements>>;
-    SrcArray const* pSrcArr = reinterpret_cast<SrcArray const*>(pSrc);
-    DstArray* pDstArr = reinterpret_cast<DstArray*>(pDst);
+    using src_dtype = std::conditional_t<num_elements >= 8, uint16_t, uint8_t>;
+    using dst_dtype = std::conditional_t<num_elements >= 8, uint32_t, uint16_t>;
+    using SrcArray =
+        std::conditional_t<num_elements >= 8,
+                           cutlass::Array<src_dtype, num_elements / 2>,
+                           cutlass::Array<src_dtype, num_elements>>;
+    using DstArray =
+        std::conditional_t<num_elements >= 8,
+                           cutlass::Array<dst_dtype, num_elements / 2>,
+                           cutlass::Array<dst_dtype, num_elements>>;
+    SrcArray const *pSrcArr = reinterpret_cast<SrcArray const *>(pSrc);
+    DstArray *pDstArr = reinterpret_cast<DstArray *>(pDst);
     if constexpr (num_elements >= 8) {
       // convert 2 FP8 elements at a time
       E5M2_to_FP16<num_elements / 2>(*pSrcArr, *pDstArr);
@@ -245,7 +247,7 @@ void convert_FP8_to_FP16(
       dst_vec = E4M3_to_FP16_chunk16(src_vec);
       CUTLASS_PRAGMA_UNROLL
       for (int j = 0; j < chunk_size; ++j) {
-        reinterpret_cast<uint16_t*>(pDst)[i * chunk_size + j] = dst_vec[j];
+        reinterpret_cast<uint16_t *>(pDst)[i * chunk_size + j] = dst_vec[j];
       }
     }
   }
