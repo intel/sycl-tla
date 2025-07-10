77f5798f119414e5d44b5fa8fb3624787d0f4eeb
taozha2 <tao2.zhang@intel.com>
2025-07-10 13:46:23 +0800

    update

--- build.sh
+++ build.sh
@@ -65,7 +65,7 @@ export OCL_ICD_VENDORS=$HOME
 
 # ================= target =================
 #target=./test/unit/cute/intel_xe/cutlass_test_unit_cute_intel_xe
-target=./examples/sycl/11_pvc_gemm_int4_streamk/11_pvc_gemm_int4_streamk
+target=./examples/sycl/02_bmg_gemm_mixed_dtype/02_bmg_gemm_f16_u4_f16
 
 cmake .. -G Ninja -DCUTLASS_SYCL_PROFILING_ENABLED=ON -DCMAKE_CUDA_HOST_COMPILER=$clang_path -DCMAKE_CXX_FLAGS_RELEASE=$1 \
 -DCUTLASS_ENABLE_SYCL=ON -DDPCPP_SYCL_TARGET=$output -DCMAKE_CXX_COMPILER=$clang_path \
