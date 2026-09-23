45ed1fe1ad28d676967106d0b63d4d7b6bedf11e
Anup Renikunta <113945574+anupren@users.noreply.github.com>
2026-09-22 21:25:50 -0700

    Update version v0.9.3 (#840)
    Co-authored-by: Jacky, Deng <jacky.deng@intel.com>

--- python/setup_cutlass.py
+++ python/setup_cutlass.py
@@ -52,7 +52,7 @@ setup_pycute.perform_setup()
 
 setup(
     name='sycl_tla_cppgen',
-    version='0.9.1',
+    version='0.9.3',
     description='SYCL*TLA Pythonic Interface based on CUTLASS',
     package_dir={'': '.'},
     packages=[
